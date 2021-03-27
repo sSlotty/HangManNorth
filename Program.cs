3fc519f70d932dbad2257324709a8ccda46e1044
Oatwant <thanathip.oat@g.swu.ac.th>
2021-03-27 22:15:25 +0700

    Test push Oat push

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace Hangman
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmLogin());
+            Console.WriteLine("Oat push");
         }
     }
 }
